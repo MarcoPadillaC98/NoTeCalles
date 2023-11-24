1cbcc4485afd189789b062abc14fe14ad187e094
MarcoPadillaC98 <44485690+MarcoPadillaC98@users.noreply.github.com>
2023-11-24 14:20:29 -0500

    Glosario Front 100%

---
+++ app/src/main/java/com/example/notecalles/activity/ui/gallery/g_CallejeroActivity.java
@@ -0,0 +1,16 @@
+package com.example.notecalles.activity.ui.gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notecalles.R;
+
+public class g_CallejeroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gcallejero);
+    }
+}
