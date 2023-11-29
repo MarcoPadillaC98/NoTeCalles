2840ac90fbd6c6264eb4269f8cd2ec511b8daee1
MarcoPadillaC98 <marcopc98@gmail.com>
2023-11-29 11:08:55 -0500

    Spinner

--- app/src/main/java/com/example/notecalles/activity/ui/publicar/PublicarFragment.java
+++ app/src/main/java/com/example/notecalles/activity/ui/publicar/PublicarFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,14 +21,26 @@ import androidx.navigation.Navigation;
 
 import com.example.notecalles.R;
 import com.example.notecalles.databinding.FragmentPublicarBinding;
+import com.example.notecalles.model.Tipo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 
 public class PublicarFragment extends Fragment{
 
     Button Calendario;
+    Spinner spn_tipos;
     EditText fecha;
+
+    DatabaseReference mDataBase;
+
     private int dia,mes,ano;
     private FragmentPublicarBinding binding;
 
@@ -37,7 +51,10 @@ public class PublicarFragment extends Fragment{
 
         binding = FragmentPublicarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
         return root;
+
+
     }
 
     @Override
@@ -49,8 +66,16 @@ public class PublicarFragment extends Fragment{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         Button Calendario = view.findViewById(R.id.imb_Calendario);
         EditText fecha=view.findViewById(R.id.etFecha);
+        Spinner spn_tipos = view.findViewById(R.id.spn_Elegirtipo);
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+
+
+        cargarTipo();
+
+
 
         final NavController navController = Navigation.findNavController(view);
 
@@ -76,6 +101,29 @@ public class PublicarFragment extends Fragment{
             }
         });
 
+    }
+
+    public void cargarTipo(){
+        List<Tipo> tipo = new ArrayList<>();
+        mDataBase.child("Tipos").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot ds: snapshot.getChildren()){
+                        String id = ds.getKey();
+                        String nombre = ds.child("nombre").getValue().toString();
+                        tipo.add(new Tipo(nombre,id));
+                    }
+                    ArrayAdapter<Tipo> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line,tipo);
+                    spn_tipos.setAdapter(arrayAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/notecalles/model/Tipo.java
@@ -0,0 +1,32 @@
+package com.example.notecalles.model;
+
+public class Tipo {
+
+    String nombre,id;
+
+    public Tipo(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
